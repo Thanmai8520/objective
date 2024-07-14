a05be2198c082aa11aae38f5c9a163e9ac7e249f
Thanmai8520 <110587354+Thanmai8520@users.noreply.github.com>
2024-07-14 23:27:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,93 +1,57 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
-const fs = require('fs');
 
 const app = express();
 const port = 3000;
 
 app.use(cors()); // Enable CORS
 
-console.log('Server Started..');
-
-app.disable('etag');
-
+// MySQL database configuration
 const pool = mysql.createPool({
   host: '28.10.146.81',
   user: 'root',
   password: 'T@bleau',
   database: 'mortgages_sv',
   connectionLimit: 10,
-  connectTimeout: 10000 // Increase the connection timeout
+  connectTimeout: 10000 // Increase the connection timeout if needed
 });
 
-app.get('/refreshVSfile', (req, res) => {
-  try {
-    pool.query('SELECT * FROM vs_switchs', function (err, results) {
+// Utility function to execute MySQL queries
+const executeQuery = (query, params) => {
+  return new Promise((resolve, reject) => {
+    pool.query(query, params, (err, results) => {
       if (err) {
-        return res.status(500).send([]);
+        reject(err);
+      } else {
+        resolve(results);
       }
-      let data = JSON.stringify(results);
-      fs.writeFile('vsflags.json', data, function (err) {
-        if (err) {
-          return res.status(500).send([]);
-        }
-        res.send({ "VSFile": "JSON file is updated" });
-      });
     });
-  } catch (error) {
-    res.status(500).send([]);
-  }
-});
-
-app.get('/getvsflags/:envname', (req, res) => {
-  let env = req.params.envname;
-  try {
-    const vsfile = fs.readFileSync('./vsflags.json', 'utf8');
-    const jsonData = JSON.parse(vsfile).filter(x => x.Env_flag === env);
-    res.send(jsonData);
-  } catch (err) {
-    res.status(500).send([]);
-  }
-});
-
-// Utility function to execute a query and get results
-const executeQuery = (query, callback) => {
-  pool.query(query, (err, results) => {
-    if (err) {
-      return callback(err, null);
-    }
-    callback(null, results);
   });
 };
-
-// GET endpoint to fetch all build details
-app.get('/mae/getBuild', (req, res) => {
-  const query = 'SELECT * FROM maebuildinfo';
-  executeQuery(query, (err, results) => {
-    if (err) {
-      res.status(500).json({ error: 'Error fetching build details' });
-    } else {
-      res.json(results);
-    }
-  });
-});
 
 // GET endpoint to fetch build details by applicationName
 app.get('/mae/getBuild/:applicationName', (req, res) => {
   const { applicationName } = req.params;
-  const query = `SELECT * FROM maebuildinfo WHERE ApplicationName = ${mysql.escape(applicationName)}`;
-  executeQuery(query, (err, results) => {
-    if (err) {
-      res.status(500).json({ error: 'Error fetching build details' });
-    } else {
+  const query = `
+    SELECT *
+    FROM maebuildinfo
+    WHERE ApplicationName = ${mysql.escape(applicationName)}
+    ORDER BY STR_TO_DATE(Date_Time, '%d/%m/%Y %H:%i:%s') DESC
+  `;
+  
+  executeQuery(query)
+    .then(results => {
       if (results.length > 0) {
         res.json(results);
       } else {
         res.status(404).json({ message: 'Build details not found for the application name' });
       }
-    }
-  });
+    })
+    .catch(err => {
+      console.error('Error fetching build details:', err);
+      res.status(500).json({ error: 'Error fetching build details' });
+    });
 });
 
 // Test database connection
