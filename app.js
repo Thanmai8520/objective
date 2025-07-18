c393c4191563d2ef3d182f209e09416ae9229927
Thanmai8520 <110587354+Thanmai8520@users.noreply.github.com>
2025-07-18 15:52:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,5 @@ function App() {
 }
 
 export default App;
+Hi team! I’m Sri Santhoshi Thanmai Kora, a CSE (Honors) graduate from KL University, Vijayawada. I also have a Diploma in Bharatanatyam and a Certificate in Kuchipudi. I enjoy sketching, cubing, and learning new things. Excited to grow and collaborate with you all!
+
